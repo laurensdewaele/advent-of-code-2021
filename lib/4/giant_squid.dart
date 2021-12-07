cb989f1db907a81ee13b54ffb84a88be60670c94
Laurens Dewaele <laurens.dewaele@gmail.com>
2021-12-07 08:22:59 +0100

    list frenzy

--- lib/4/giant_squid.dart
+++ lib/4/giant_squid.dart
@@ -10,8 +10,44 @@ void main(List<String> args) async {
   final input = await mapInput(lines);
   final draws = input.draws;
   final boards = input.boards;
-  print(boards);
-  print(draws);
+
+  // Go through 5 draws and iterate
+  // find if full column or full row
+  // Add another draw
+  // find if full column or full row
+
+  // Board no. winners
+  final List<int> winners = [];
+  var takeCount = 5;
+  var currentDraw = draws.take(takeCount).toList();
+
+  while (winners.isEmpty) {
+    for (var i = 0; i < boards.length; i++) {
+      final boardNo = i + 1;
+      final bool validColumns = checkColumns(currentDraw, boards[i]);
+      final bool validRows = checkRows(currentDraw, boards[i]);
+      if (validRows || validColumns) {
+        winners.add(boardNo);
+      }
+    }
+    takeCount++;
+    currentDraw = draws.take(takeCount).toList();
+  }
+}
+
+bool checkColumns(List<int> draw, List<List<int>> board) {
+  final List<List<int>> inverted = [[], [], [], [], []];
+  for (var row = 0; row < 5; row++) {
+    for (var col = 0; col < 5; col++) {
+      inverted[col].add(board[row][col]);
+    }
+  }
+
+  return checkRows(draw, inverted);
+}
+
+bool checkRows(List<int> draw, List<List<int>> board) {
+  return false;
 }
 
 Future<Input> mapInput(Stream<String> lines) async {
